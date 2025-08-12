92d69344a2838aa0cd849dd3f8852aa726318bfc
hiba <hnainehiba@gmail.com>
2025-08-12 12:10:13 +0200

    2 commit

--- week_1/day_1/daily_challengs/daily_challenge.py
+++ week_1/day_1/daily_challengs/daily_challenge.py
@@ -1,4 +1,15 @@
 # ------ Challenge 1
-
+number=int(input("enter un nombre :"))
+length = int(input("Entrez une longueur:"))
+for i in range(1,length+1):
+    numebres=number*i
+    print(f"{numebres}")
 # ------ Challenge 2
+str=input("entrer un string")
+newStr=""
+for i in range (len(str)):
+   if i == len(str) - 1 or str[i] != str[i + 1]:
+        newStr+=str[i]
+print(f"{newStr}")
 
+    
--- week_1/day_1/exercises/exercises_xp_gold.py
+++ week_1/day_1/exercises/exercises_xp_gold.py
@@ -1,15 +1,74 @@
 # ------ Exercise 1
-
+#1
+month = int(input("Entrez un mois (1 à 12) : "))
+if 1 <= month <= 12:
+    print(f"Vous avez choisi le mois numéro {month}.")
+else:
+    print("Mois invalide. Veuillez entrer un nombre entre 1 et 12.")
+#2
+if month>=3 and month<=5:
+     print("Spring")
+elif month>=6 and month<=8:
+    print("summer")
+elif month>=9 and month<=11:
+    print("Autumn")
+elif month>=12 and month<=2:
+    print("Winter")   
+     
 # ------ Exercise 2
+#1
+for number in range(1, 21):
+    print(number)
+#2
+numbers = range(1, 21) 
+for index, value in enumerate(numbers):
+    if index % 2 == 0:
+        print(value)
 
 # ------ Exercise 3
+while name:=Hiba :
+    print 
+my_name = "Hiba" 
+while True:
+    name = input("Entrez votre nom : ")
+    if name == my_name:
+        print("Bonjour, nous avons le même nom !")
+        break 
+    else:
+        print("Ce n'est pas mon nom, essayez encore.")
 
 # ------ Exercise 4
+names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+user_name = input("Entrez votre nom : ")
+if user_name in names:
+    index = names.index(user_name)
+    print(f"Le nom '{user_name}' se trouve à l'index {index}.")
+else:
+    print(f"Le nom '{user_name}' n'est pas dans la liste.")
 
 # ------ Exercise 5
+numbers=[]
+for i in range(1,4):
+ num = float(input(f"Entrez le {i}ème nombre : "))
+ numbers.append(num)
+ #2
+ greatest = numbers[0] 
+for num in numbers:
+    if num > greatest:
+        greatest = num
+print(f"The greatest number is:{greatest}")
 
 # ------ Exercise 6
-
+import random
+random_number = random.randint(1, 9)
+user_number = int(input("Entrez un nombre entre 1 et 9 : "))
+print(f"Votre nombre : {user_number}")
+random_number = random.randint(1, 9)
+print(f"Votre nombre : {random_number}")
+if user_number==random_number:
+    print("Winner")
+else:
+    print("Better luck next time")
 # ------ Exercise 7
 
 # ------ Exercise 8
