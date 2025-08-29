32350970738333dcd31c358a674efe5defe7685f
hiba <hnainehiba@gmail.com>
2025-08-29 12:36:46 +0200

    daily challenge week2

---
+++ week_2/day_1/daily_challenge/Daily Challenge.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) AS total_actors
+FROM actors;
+INSERT INTO actors (first_name, last_name,birthdate, number_oscars)
+VALUES ('Leonardo', 'DiCaprio', '1974-11-11', 1);
+select * from actors
+
