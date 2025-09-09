3b6806e1474507d7ed7399495a5a43b5e56ca4ee
hiba <hnainehiba@gmail.com>
2025-09-09 11:30:16 +0200

    day 4 daily challenge commit

---
+++ week_2/day_4/daily_challenge/Daily.py
@@ -0,0 +1,43 @@
+import requests
+import psycopg2
+import random
+
+# ✅ Connexion à PostgreSQL (avec tes vrais paramètres)
+conn = psycopg2.connect(
+    dbname="new_database",
+    user="postgres",
+    password="1234hihi",
+    host="localhost",
+    port="5432"
+)
+cur = conn.cursor()
+
+# 1. Récupérer tous les pays via API
+url = "https://restcountries.com/v3.1/all?fields=name,capital,flag,subregion,population"
+response = requests.get(url)
+countries = response.json()
+
+# 2. Choisir 10 pays au hasard
+random_countries = random.sample(countries, 10)
+
+# 3. Insérer dans la base
+for country in random_countries:
+    name = country.get("name", {}).get("common", "Unknown")
+    capital = country.get("capital", ["Unknown"])[0] if country.get("capital") else "Unknown"
+    flag = country.get("flag", "🏳️")
+    subregion = country.get("subregion", "Unknown")
+    population = country.get("population", 0)
+
+    cur.execute("""
+        INSERT INTO countries (name, capital, flag, subregion, population)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (name, capital, flag, subregion, population))
+
+# Sauvegarder les changements
+conn.commit()
+
+print("10 random countries inserted successfully!")
+
+# Fermer la connexion
+cur.close()
+conn.close()
