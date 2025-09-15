a923b7d5e20da5cdef59919cfc04f9855de4fd85
hiba <hnainehiba@gmail.com>
2025-09-15 17:48:11 +0200

    dya4 challenges commit

--- week_3/day_2/daily_challenge/challenges.js
+++ week_3/day_2/daily_challenge/challenges.js
@@ -1,5 +1,13 @@
 // ====== Daily Challenge 1
-
+let sentence = "This dinner is not so bad! You cook well";
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+    let newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+    console.log(newSentence);
+  } else {
+    console.log(sentence);
+  }
 // ====== Daily Challenge 2
 
 // ====== Daily Challenge 3
