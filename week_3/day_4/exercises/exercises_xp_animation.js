6699eebbbc848bbf53b4867f7c4082bb6f7c0a7a
hiba <hnainehiba@gmail.com>
2025-09-26 16:38:04 +0100

    exercisesxp animation w3 day4 commit

--- week_3/day_4/exercises/exercises_xp_animation.js
+++ week_3/day_4/exercises/exercises_xp_animation.js
@@ -1,7 +1,54 @@
 // ===== Exercise 1
-
+// Part I
+//
+setTimeout(function() {
+    alert("Hello World");
+  }, 2000);
+  
+  // Part II 
+  setTimeout(function() {
+    let container = document.getElementById("container");
+    let p = document.createElement("p");
+    p.textContent = "Hello World";
+    container.appendChild(p);
+  }, 2000);
+  
+  // Part III
+  let container = document.getElementById("container");
+  let button = document.getElementById("clear");
+  
+  let count = 0;
+  //
+  let interval = setInterval(function() {
+    let p = document.createElement("p");
+    p.textContent = "Hello World";
+    container.appendChild(p);
+    count++;
+    // 
+    if (count >= 5) {
+      clearInterval(interval);
+    }
+  }, 2000);
+  // 
+  button.addEventListener("click", function() {
+    clearInterval(interval);
+  });
+  
 // ===== Exercise 2
-
+function myMove() {
+    let elem = document.getElementById("animate");
+    let pos = 0; // البداية
+    let id = setInterval(frame, 1); 
+    function frame() {
+      if (pos >= 350) { 
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.left = pos + "px";
+      }
+    }
+  }
+  
 // ===== Exercise 3
 
 // ===== Exercise 4
