ed49139ff1df61567a9d6ec7d3c15bc5f2b04fdc
hiba <hnainehiba@gmail.com>
2025-10-05 11:58:11 +0100

    day4 week 3 challenge2 commit

--- week_3/day_4/daily_challenge/challenge_2.js
+++ week_3/day_4/daily_challenge/challenge_2.js
@@ -1,1 +1,5 @@
-
+// récupère l'input 
+const input = document.getElementById('letterInput');
+input.addEventListener('keyup', function() {
+  this.value = this.value.replace(/[^a-zA-Z]/g, '');
+});
