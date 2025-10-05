12f751c394953b3149b1f929070aa8caa0e0c559
hiba <hnainehiba@gmail.com>
2025-10-05 13:43:15 +0100

    day1 week 4 Exe ninja commit

---
+++ week_4/day_1/exercises/Exercises XP Ninja.js
@@ -0,0 +1,7 @@
+const mergeWords = string => nextString =>
+    nextString === undefined
+      ? string
+      : mergeWords(`${string} ${nextString}`);
+      console.log(mergeWords('There')('is')('no')('spoon.')());
+    
+        
