7727d0af5ec90f9caddc4571c83f50acc9833acb
hiba <hnainehiba@gmail.com>
2025-10-06 21:47:49 +0100

    day 3 week4 Exercises XP ninja commit

---
+++ week_4/day_3/exercises/Exercises XP Ninja.js
@@ -0,0 +1,4 @@
+//Flamingo extends Bird, so calling super() runs the parent constructor.
+//The order of execution follows the order of the statements in Flamingo:
+//Print “I’m pink”
+//Call super() => print “I’m a bird”
